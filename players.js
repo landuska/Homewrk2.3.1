c4f0992507db7a8ddf3c7173ad98dbc31e3bda4a
Landysh Khusaenova <landysh.kg@gmail.com>
2023-10-23 23:40:23 +0200

    Homework 2.3.1_updates file

--- players.js
+++ players.js
@@ -3,6 +3,8 @@ const players = require('./data.json');
 let score = [];
 let gamerName = [];
 
+// Создаем два массива: один - с именами игроков, другой - с очками
+
 for (let i in players) {
     gamerName.push(players[i].name);
     score.push(players[i].scorepoint);
@@ -10,7 +12,11 @@ for (let i in players) {
 
 console.log(score, gamerName);
 
+// Ищем максимальное количество баллов
+
 let maxScore = Math.max(...score);
+
+// Выводим в консоль игрока, которому пренадлежит максимальное количество баллов
 
 for (let j in gamerName) {
    if (score.indexOf (maxScore, 0) == j) {
