ae1ced2e264d3719fcbc4cdec1c164ccae5a4de2
Landysh Khusaenova <landysh.kg@gmail.com>
2023-10-26 20:31:46 +0200

    one more option to calculate score

---
+++ likes.js
@@ -0,0 +1,8 @@
+let likes = 0;
+let like = true;
+
+if (like) {
+likes++
+}
+
+console.log(likes)
--- players.js
+++ players.js
@@ -13,14 +13,27 @@ for (let i in players) {
 console.log(score, gamerName);
 
 // Ищем максимальное количество баллов
+// let maxScore = Math.max(...score);
 
-let maxScore = Math.max(...score);
+// сортируем массив по возрастанию
+function compareNumbers(a, b) {
+  return a - b;
+};
+
+console.log(score.sort(compareNumbers));
+let maxScore = score.pop()
 
 // Выводим в консоль игрока, которому пренадлежит максимальное количество баллов
 
-for (let j in gamerName) {
-  if (score.indexOf(maxScore, 0) == j) {
+// for (let j in gamerName) {
+//   if (score.indexOf(maxScore, 0) == j) {
+//     console.log(`Максимальное количество очков: ${gamerName[j]} - ${maxScore}`);
+//   };
+// };
+
+for (let j in players) {
+  if (players[j].scorepoint == maxScore) {
     console.log(`Максимальное количество очков: ${gamerName[j]} - ${maxScore}`);
-    break;
-  }
+  };
 }
+
